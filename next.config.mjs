9de8e6fa51f58e86d42a877c3277d820d02b055d
Thomas Askharoun <151990913+askharoun@users.noreply.github.com>
2025-05-15 04:18:48 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // GitHub Pages adds a trailing slash by default that needs to be handled
   trailingSlash: true,
-  // The base path should match your GitHub repository name if it's not served from the root
-  // e.g., if your repo is username.github.io/my-project, use basePath: '/my-project'
-  // basePath: '/your-repo-name',
+  
+  basePath: '/askharoun.dev-FINAL,
   assetPrefix: './',
 }
 
